a7bbfbd3f1c79a5bd59e201cff3af85f1b877965
Emete Gift <emeteokeoghene@gmail.com>
2023-07-08 16:51:54 +0100

    Installed django rest_framework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,28 @@
 # Create the API endpoint view
 import json
-from django.http import JsonResponse
+from rest_framework.response import Response
+from django.forms.models import model_to_dict
 from products.models import Product
+from rest_framework.decorators import api_view
+
 
 # define the function views
-
+@api_view(["GET"])
 def api_home(request, *args, **kwargs ): 
+
+    """
+    This is a Django Rest API view
+    """
     model_data = Product.objects.all().order_by("?").first()
     data ={}
-    data['title'] = model_data.title
-    data['content'] = model_data.content
-    data['price'] =model_data.price
-    return JsonResponse(data)
+    # # The code below takes a model instance i.e, model_data in this case, turn it into a python dictionary and returns a JSON to the client
+    # data['title'] = model_data.title
+    # data['content'] = model_data.content
+    # data['price'] =model_data.price
+
+    ## The code commented above can simply be written as follow using the model_to_dict function
+
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return Response(data)
+
