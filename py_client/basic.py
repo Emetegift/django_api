07a06be8c4a5836ef1ec5d341fe8714a7b1c3c70
Emete Gift <emeteokeoghene@gmail.com>
2023-07-08 14:50:49 +0100

    Connected the project to the local server

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,8 @@ import requests
 
 # This is a proces used to access external APIs
 # enpoint ="https://httpbin.org/status/200"
-enpoint ="https://httpbin.org/anything"
+# enpoint ="https://httpbin.org/anything"
+enpoint ="http://localhost:8000/"
 
 get_response = requests.get(enpoint, json={"query": "Hello world"}) # This http request will return the data in a json format thats similar to a python dictionary
 print(get_response.text)
@@ -10,5 +11,5 @@ print(get_response.text)
 #HTTP Request---> returns HTML, while
 #REST API HTTP requests ---> JSON
 
-print(get_response.json())
-print(get_response.status_code)
+# print(get_response.json())
+# print(get_response.status_code)
