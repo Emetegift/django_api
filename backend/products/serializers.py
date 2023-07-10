1edf86afd7a2ae18d295c0e1233fb643f4c267bb
Emete Gift <emeteokeoghene@gmail.com>
2023-07-10 14:30:38 +0100

    Added urls to serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 from .models import Product
+from rest_framework.reverse import reverse
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url =  serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(  #This will handle the detail view (retrieve)
+        view_name='product-detail',
+        lookup_field = 'pk'
+    )
     class Meta:
         model = Product
         fields =[
+            'url',
+            'edit_url',
             'pk',
             'title',
             'content',
@@ -14,6 +22,13 @@ class ProductSerializer(serializers.ModelSerializer):
             'my_discount'
         ]
 
+    def get_edit_url(self, obj):
+         # return f"/api/v2/products/{obj.pk}"  # OR
+        request = self.context.get('request') # This is a get request because it is how serializers work 
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk":obj.pk}, request=request) # This will handle the update endpoint
+        # return reverse("product-detail", kwargs={"pk":obj.pk}, request=request)
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
             return None
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 
 urlpatterns=[
-    path('', views.ProductListCreateAPIView.as_view()),
+    path('', views.ProductListCreateAPIView.as_view(), name="product-list"),
     # path('', views. product_alt_view), # This url is for the single function endpoints for list and create
     # path('<int:pk>/', views. product_alt_view), #This url is for the single function endpoints for retrieve
-    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
+    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view(), name ="product-edit"),
     # path('', views.ProductMixin.as_view()),# Url for create and get list method using the generic class view
     path('<int:pk>/delete/', views.ProductDestroyAPIView.as_view()),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
+    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name="product-detail"),
     #  path('<int:pk>/', views.ProductMixin.as_view()), # Url for detail  get (by_id) method using the generic class view
 ]
 
