01e3fd413571ee8cc4e1d53194ff0130a173d1c1
Emete Gift <emeteokeoghene@gmail.com>
2023-07-10 20:55:21 +0100

    Created  custom validation with serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Product
 from rest_framework.reverse import reverse
+# from .validators import validate_title
+from . import validators
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
@@ -9,7 +11,10 @@ class ProductSerializer(serializers.ModelSerializer):
         view_name='product-detail',
         lookup_field = 'pk'
     )
-    # email = serializers.EmailField(write_only=True)
+    # email = serializers.EmailField(write_only=True) # Please note that this can be anything besides email
+    # title = serializers.CharField(validators=[validate_title])
+    title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
+    # email = serializers.CharField(source='user.email', read_only=True)
     class Meta:
         model = Product
         fields =[
@@ -18,12 +23,21 @@ class ProductSerializer(serializers.ModelSerializer):
             # 'email',
             'pk',
             'title',
+            # 'name',
             'content',
             'price',
             'sale_price',
             'my_discount'
         ]
- 
+
+    # # To validate a particular value in a database, e.g;
+    # def validate_title(self, value):
+    #     request = self.context.get(request)
+    #     user = request.user
+    #     qs = Product.objects.filter(title__iexact=value) # This will querry the exact value in the database
+    #     if qs.exists():
+    #         raise serializers.ValidationError(F"{value} is already a product name.")
+    #     return value 
 #  # This is basically how a new field can be added and updated using serializers
 #     def create(self, validated_data):
 #     #   return  Product.objects.create(**validated_data) OR
