bcdcb2ea7c3b04c88f92372d9979edb71f27f1bc
Emete Gift <emeteokeoghene@gmail.com>
2023-07-14 17:06:30 +0100

    Search via Rest API &JS client

--- js_client/client.js
+++ js_client/client.js
@@ -1,9 +1,17 @@
 const contentContainer = document.getElementById('content-container')
+
 const loginForm = document.getElementById('login-form')
+
+const searchForm = document.getElementById('search-form')
 const baseEndpoint = "http://localhost:8000/api"
 if (loginForm){
     // handle this login form,i.e it should submit this form if its a login form
     loginForm.addEventListener('submit', handleLogin)
+}
+
+if (searchForm){
+    // handle this login form,i.e it should submit this form if its a login form
+    searchForm.addEventListener('submit', handleSearch)
 }
 
 function handleLogin(event) {
@@ -32,6 +40,55 @@ function handleLogin(event) {
         console.log('err', err)
     })
 }
+
+function handleSearch(event) {
+    event.preventDefault()
+    let formData = new FormData(searchForm)
+    let data = Object.fromEntries(formData)
+    let searchParams = new URLSearchParams(data)
+    const endpoint = `${baseEndpoint}/search/?${searchParams}`
+    const headers = {
+        "Content-Type": "application/json",
+    }
+    const authToken = localStorage.getItem('access')
+    if (authToken){
+        headers['Authorization'] =`Bearer ${authToken}`
+    }
+    const options = {
+        method : "GET",
+        headers :headers
+
+    }
+    fetch(endpoint, options) // This fetch is running the request.Posts endpoint called the promise
+    .then(response=>{
+        return response.json()
+    })
+    .then(data=>{
+        // writeToContainer(data)
+        const validData = isTokenNotValid(data)
+        if (validData && contentContainer){
+            contentContainer.innerHTML = ""
+            if (data && data.hits) {
+                let htmlStr = ""
+                for (let result of data.hits) {
+                    htmlStr += "<li>"+ result.title + "</li>"
+                }
+                contentContainer.innerHTML = htmlStr
+                if (data.hits.length===0) {
+                    contentContainer.innerHTML = "<p>No results found</p>"
+                }
+            } else {
+                contentContainer.innerHTML = "<p> No results found </p>"
+            }
+        }
+        
+    })
+    .catch(err=>{
+        console.log('err', err)
+    })
+}
+
+
 
 function handleAuthData(authData, callback) {
     localStorage.setItem('access', authData.access) //This will handle the access token request
@@ -102,3 +159,5 @@ function getProductList(){
         
     })  
 }   
+validateJWTToken()
+// handleSearch()
